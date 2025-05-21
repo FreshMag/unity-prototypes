f6da32511da89fdb46b9d6f18d0dacbc905d24c6
FreshMag <pesco.magnani@gmail.com>
2025-05-21 14:59:19 +0200

    feat: added camera rotation system

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -3,13 +3,50 @@ using UnityEngine;
 public class CameraSwitch : MonoBehaviour
 {
     public Camera newCamera;
+    public float transitionDuration = 1.0f;
+
+    private float t = 0f;
+    private bool isTransitioning = false;
+    private Vector3 newPosition;
+    private Quaternion newRotation;
+    private float newFov;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && newCamera != Camera.main)
         {
-            Camera.main.gameObject.SetActive(false);
-            newCamera.gameObject.SetActive(true);
+            StartTransition(Camera.main);
         }
+    }
+
+    void Update()
+    {
+        if (isTransitioning)
+        {
+            t += Time.deltaTime / transitionDuration;
+            newCamera.transform.position = Vector3.Lerp(newCamera.transform.position, newPosition, t);
+            newCamera.transform.rotation = Quaternion.Lerp(newCamera.transform.rotation, newRotation, t);
+            newCamera.fieldOfView = Mathf.Lerp(newCamera.fieldOfView, newFov, t);
+
+            if (t >= 1f)
+            {
+                isTransitioning = false;
+            }
+        }
+    }
+
+    public void StartTransition(Camera oldCamera)
+    {
+        newPosition = newCamera.transform.position;
+        newRotation = newCamera.transform.rotation;
+        newFov = newCamera.fieldOfView;
+
+        newCamera.transform.position = oldCamera.transform.position;
+        newCamera.transform.rotation = oldCamera.transform.rotation;
+        newCamera.fieldOfView = oldCamera.fieldOfView;
+        t = 0f;
+        isTransitioning = true;
+        newCamera.gameObject.SetActive(true);
+        oldCamera.gameObject.SetActive(false);
     }
 }
